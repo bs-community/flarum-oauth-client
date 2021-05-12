872c4d88ff49dbc75038cc15013c8815ab20329b
Pig Fang <g-plane@hotmail.com>
2021-05-12 09:39:52 +0800

    refactor js build

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,1 +1,28 @@
-module.exports = require('flarum-webpack-config')();
+const path = require('path')
+
+/** @type {import('webpack').Configuration} */
+const config = {
+  entry: {
+    forum: './forum.js',
+    admin: './admin.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    library: {
+      type: 'assign',
+      name: 'module.exports',
+    },
+  },
+  externals: [
+    // copied and modified from "flarum-webpack-config"
+    function ({ request }, callback) {
+      const matches = /^flarum\/(.+)$/.exec(request)
+      if (matches) {
+        return callback(null, "root flarum.core.compat['" + matches[1] + "']")
+      }
+      callback()
+    },
+  ],
+}
+
+module.exports = config
